3efd6c815124ca95f223ec779a1789ff5fda35d1
samratrocks <samrat.shiwakoti@gmail.com>
2020-10-26 21:16:23 -0400

    cleanup.

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,4 @@
 "use strict";
-const l = console.log;
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 const Database = use("Database");
@@ -20,10 +6,7 @@ const Database = use("Database");
 // Task related routes
 Route.post("/task", async ({ request, response }) => {
     const data = request.all();
-    console.log(data);
-    // const data = request.only(['id', 'name', 'status']);
     const { id, name, status } = data;
-    console.log(name);
 
     // If id is passed in, we update not insert
     if (id !== undefined) {
